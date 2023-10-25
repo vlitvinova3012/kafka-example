238adc12d03d4735ed1739451e4b7f0310e85a64
Litvinova Viktoria <vlitvinova@diasoft.ru>
2023-10-25 14:19:40 +0300

    Initial commit

--- src/main/java/com/example/kafkaexample/listener/DemoListener.java
+++ src/main/java/com/example/kafkaexample/listener/DemoListener.java
@@ -1,4 +1,4 @@
-package com.example.kafkaexample.lstener;
+package com.example.kafkaexample.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
